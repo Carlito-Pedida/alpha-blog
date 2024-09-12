5b674ece364cab6154d1498ac147c425a76e63b7
Carlito Pedida Jr <carlped417@gmail.com>
2024-09-12 09:10:53 -0500

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,22 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+           flash[:notice] = "Your account was updated successfully"
+           redirect_to root_path
+        else
+            render "new"
+        end
+    end
+   
+
     def create
         @user = User.new(user_params)
         if @user.save
